f787fbc2849e97c41ca7763d4239daabff8e345d
ErikBoesen <me@erikboesen.com>
2018-01-09 08:48:43 -0500

    Clean up ui.js

--- src/ui.js
+++ src/ui.js
@@ -28,74 +28,66 @@ let ui = {
     autoSelect: document.getElementById('auto-select'),
     armPosition: document.getElementById('arm-position')
 };
+
 let address = document.getElementById('connect-address'),
     connect = document.getElementById('connect');
 
-// Sets function to be called on NetworkTables connect. Commented out because it's usually not necessary.
-// NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
+// Set function to be called on NetworkTables connect. Usually not necessary.
+//NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
 
-// Sets function to be called when robot dis/connects
+// Set function to be called when robot dis/connects
 NetworkTables.addRobotConnectionListener(onRobotConnection, false);
 
 // Sets function to be called when any NetworkTables key/value changes
 NetworkTables.addGlobalListener(onValueChanged, true);
 
-// Function for hiding the connect box 
+// Function for hiding the connect box
 let escCount = 0;
 onkeydown = key => {
     if (key.key === 'Escape') {
         setTimeout(() => { escCount = 0; }, 400);
         escCount++;
-        console.log(escCount);
-        if (escCount === 2) {
-            document.body.classList.toggle('login-close', true);
-        }
+        if (escCount === 2) document.body.classList.toggle('login-close', true);
     }
-    else
-        console.log(key.key);
+    else console.log(key.key);
 };
-if (noElectron) {
-    document.body.classList.add('login-close');
-}
 
 /**
  * Function to be called when robot connects
- * @param {boolean} connected 
+ * @param {boolean} connected
  */
 function onRobotConnection(connected) {
     var state = connected ? 'Robot connected!' : 'Robot disconnected.';
     console.log(state);
     ui.robotState.data = state;
-    if (!noElectron) {
-        if (connected) {
-            // On connect hide the connect popup
-            document.body.classList.toggle('login-close', true);
-        }
-        else {
-            // On disconnect show the connect popup
-            document.body.classList.toggle('login-close', false);
-            // Add Enter key handler
-            address.onkeydown = ev => {
-                if (ev.key === 'Enter') {
-                    connect.click();
-                }
-            };
-            // Enable the input and the button
-            address.disabled = false;
-            connect.disabled = false;
-            connect.firstChild.data = 'Connect';
-            // Add the default address and select xxxx
-            address.value = 'roborio-xxxx.local';
-            address.focus();
-            address.setSelectionRange(8, 12);
-            // On click try to connect and disable the input and the button
-            connect.onclick = () => {
-                ipc.send('connect', address.value);
-                address.disabled = true;
-                connect.disabled = true;
-                connect.firstChild.data = 'Connecting';
-            };
-        }
+    if (connected) {
+        // On connect hide the connect popup
+        document.body.classList.toggle('login-close', true);
+    }
+    else {
+        // On disconnect show the connect popup
+        document.body.classList.toggle('login-close', false);
+        // Add Enter key handler
+        address.onkeydown = ev => {
+            if (ev.key === 'Enter') {
+                connect.click();
+            }
+        };
+        // Enable the input and the button
+        address.disabled = false;
+        connect.disabled = false;
+        connect.firstChild.data = 'Connect';
+        // Add the default address and select xxxx
+        address.value = 'roborio-xxxx.local';
+        address.focus();
+        address.setSelectionRange(8, 12);
+        // On click try to connect and disable the input and the button
+        connect.onclick = () => {
+            ipc.send('connect', address.value);
+            address.disabled = true;
+            connect.disabled = true;
+            connect.firstChild.data = 'Connecting';
+        };
     }
 }
 
@@ -114,7 +106,6 @@ let updateGyro = (key, value) => {
 NetworkTables.addKeyListener('/SmartDashboard/drive/navx/yaw', updateGyro);
 
 // The following case is an example, for a robot with an arm at the front.
-// Info on the actual robot that this works with can be seen at thebluealliance.com/team/1418/2016.
 NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
     // 0 is all the way back, 1200 is 45 degrees forward. We don't want it going past that.
     if (value > 1140) {
@@ -140,45 +131,10 @@ NetworkTables.addKeyListener('/SmartDashboard/example_variable', (key, value) =>
     ui.example.readout.data = 'Value is ' + (value ? 'true' : 'false');
 });
 
-NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
-    // Sometimes, NetworkTables will pass booleans as strings. This corrects for that.
-    if (typeof value === 'string')
-        value = value === 'true';
-    // When this NetworkTables variable is true, the timer will start.
-    // You shouldn't need to touch this code, but it's documented anyway in case you do.
-    var s = 135;
-    if (value) {
-        // Make sure timer is reset to black when it starts
-        ui.timer.style.color = 'black';
-        // Function below adjusts time left every second
-        var countdown = setInterval(function () {
-            s--; // Subtract one second
-            // Minutes (m) is equal to the total seconds divided by sixty with the decimal removed.
-            var m = Math.floor(s / 60);
-            // Create seconds number that will actually be displayed after minutes are subtracted
-            var visualS = (s % 60);
-            // Add leading zero if seconds is one digit long, for proper time formatting.
-            visualS = visualS < 10 ? '0' + visualS : visualS;
-            if (s < 0) {
-                // Stop countdown when timer reaches zero
-                clearTimeout(countdown);
-                return;
-            }
-            else if (s <= 15) {
-                // Flash timer if less than 15 seconds left
-                ui.timer.style.color = (s % 2 === 0) ? '#FF3030' : 'transparent';
-            }
-            else if (s <= 30) {
-                // Solid red timer when less than 30 seconds left.
-                ui.timer.style.color = '#FF3030';
-            }
-            ui.timer.firstChild.data = m + ':' + visualS;
-        }, 1000);
-    }
-    else {
-        s = 135;
-    }
-    NetworkTables.putValue(key, false);
+NetworkTables.addKeyListener('/robot/time', (key, value) => {
+    // This is an example of how a dashboard could display the remaining time in a match.
+    // We assume here that value is an integer representing the number of seconds left.
+    ui.timer.innerHTML = value < 0 ? '0:00' : Math.floor(value / 60) + ':' + (value % 60 < 10 ? '0' : '') + value % 60;
 });
 
 // Load list of prewritten autonomous modes
@@ -204,9 +160,9 @@ NetworkTables.addKeyListener('/SmartDashboard/autonomous/selected', (key, value)
 
 /**
  * Global Listener that runs whenever any value changes
- * @param {string} key 
- * @param value 
- * @param {boolean} isNew 
+ * @param {string} key
+ * @param value
+ * @param {boolean} isNew
  */
 function onValueChanged(key, value, isNew) {
     // Sometimes, NetworkTables will pass booleans as strings. This corrects for that.
@@ -238,21 +194,21 @@ function onValueChanged(key, value, isNew) {
             if (typeof value === 'boolean') {
                 input.type = 'checkbox';
                 input.checked = value; // value property doesn't work on checkboxes, we'll need to use the checked property instead
-                input.onchange = function () {
+                input.onchange = function() {
                     // For booleans, send bool of whether or not checkbox is checked
                     NetworkTables.putValue(key, this.checked);
                 };
             }
             else if (!isNaN(value)) {
                 input.type = 'number';
-                input.onchange = function () {
+                input.onchange = function() {
                     // For number values, send value of input as an int.
                     NetworkTables.putValue(key, parseInt(this.value));
                 };
             }
             else {
                 input.type = 'text';
-                input.onchange = function () {
+                input.onchange = function() {
                     // For normal text values, just send the value.
                     NetworkTables.putValue(key, this.value);
                 };
@@ -265,33 +221,27 @@ function onValueChanged(key, value, isNew) {
         // Find already-existing input for changing this variable
         var oldInput = document.getElementsByName(propName)[0];
         if (oldInput) {
-            if (oldInput.type === 'checkbox') {
-                oldInput.checked = value;
-            }
-            else {
-                oldInput.value = value;
-            }
+            if (oldInput.type === 'checkbox') oldInput.checked = value;
+            else oldInput.value = value;
         }
-        else {
-            console.log('Error: Non-new variable ' + key + ' not present in tuning list!');
-        }
+        else console.log('Error: Non-new variable ' + key + ' not present in tuning list!');
     }
 }
 
 // The rest of the doc is listeners for UI elements being clicked on
-ui.example.button.onclick = function () {
+ui.example.button.onclick = function() {
     // Set NetworkTables values to the opposite of whether button has active class.
     NetworkTables.putValue('/SmartDashboard/example_variable', this.className != 'active');
 };
 // Reset gyro value to 0 on click
-ui.gyro.container.onclick = function () {
+ui.gyro.container.onclick = function() {
     // Store previous gyro val, will now be subtracted from val for callibration
     ui.gyro.offset = ui.gyro.val;
     // Trigger the gyro to recalculate value.
     updateGyro('/SmartDashboard/drive/navx/yaw', ui.gyro.val);
 };
 // Open tuning section when button is clicked
-ui.tuning.button.onclick = function () {
+ui.tuning.button.onclick = function() {
     if (ui.tuning.list.style.display === 'none') {
         ui.tuning.list.style.display = 'block';
     }
@@ -300,20 +250,20 @@ ui.tuning.button.onclick = function () {
     }
 };
 // Manages get and set buttons at the top of the tuning pane
-ui.tuning.set.onclick = function () {
+ui.tuning.set.onclick = function() {
     // Make sure the inputs have content, if they do update the NT value
     if (ui.tuning.name.value && ui.tuning.value.value) {
         NetworkTables.putValue('/SmartDashboard/' + ui.tuning.name.value, ui.tuning.value.value);
     }
 };
-ui.tuning.get.onclick = function () {
+ui.tuning.get.onclick = function() {
     ui.tuning.value.value = NetworkTables.getValue(ui.tuning.name.value);
 };
 // Update NetworkTables when autonomous selector is changed
-ui.autoSelect.onchange = function () {
+ui.autoSelect.onchange = function() {
     NetworkTables.putValue('/SmartDashboard/autonomous/selected', this.value);
 };
 // Get value of arm height slider when it's adjusted
-ui.armPosition.oninput = function () {
+ui.armPosition.oninput = function() {
     NetworkTables.putValue('/SmartDashboard/arm/encoder', parseInt(this.value));
 };
