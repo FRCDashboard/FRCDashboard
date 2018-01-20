4e955d4e06bca5cff87a3add1c833c66ad1b3dbb
ErikBoesen <me@erikboesen.com>
2018-01-20 15:01:46 -0500

    Split connection logic from ui.js into connection.js
    Fix #38

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,3 @@
-// Import ipc
-ipc = require('electron').ipcRenderer;
-
 // Define UI elements
 let ui = {
     timer: document.getElementById('timer'),
@@ -32,69 +29,7 @@ let ui = {
     armPosition: document.getElementById('arm-position')
 };
 
-let address = document.getElementById('connect-address'),
-    connect = document.getElementById('connect');
-
-// Set function to be called on NetworkTables connect. Usually not necessary.
-//NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
-
-// Set function to be called when robot dis/connects
-NetworkTables.addRobotConnectionListener(onRobotConnection, false);
-
-// Sets function to be called when any NetworkTables key/value changes
-NetworkTables.addGlobalListener(onValueChanged, true);
-
-// Function for hiding the connect box
-let escCount = 0;
-onkeydown = key => {
-    if (key.key === 'Escape') {
-        setTimeout(() => { escCount = 0; }, 400);
-        escCount++;
-        if (escCount === 2) document.body.classList.toggle('login-close', true);
-    }
-    else console.log(key.key);
-};
-
-/**
- * Function to be called when robot connects
- * @param {boolean} connected
- */
-function onRobotConnection(connected) {
-    var state = connected ? 'Robot connected!' : 'Robot disconnected.';
-    console.log(state);
-    ui.robotState.data = state;
-    if (connected) {
-        // On connect hide the connect popup
-        document.body.classList.toggle('login-close', true);
-    }
-    else {
-        // On disconnect show the connect popup
-        document.body.classList.toggle('login-close', false);
-        // Add Enter key handler
-        address.onkeydown = ev => {
-            if (ev.key === 'Enter') {
-                connect.click();
-            }
-        };
-        // Enable the input and the button
-        address.disabled = false;
-        connect.disabled = false;
-        connect.firstChild.data = 'Connect';
-        // Add the default address and select xxxx
-        address.value = 'roborio-xxxx.local';
-        address.focus();
-        address.setSelectionRange(8, 12);
-        // On click try to connect and disable the input and the button
-        connect.onclick = () => {
-            ipc.send('connect', address.value);
-            address.disabled = true;
-            connect.disabled = true;
-            connect.firstChild.data = 'Connecting';
-        };
-    }
-}
-
-/**** KEY Listeners ****/
+// Key Listeners
 
 // Gyro rotation
 let updateGyro = (key, value) => {
