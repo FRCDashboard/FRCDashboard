179d172e822575136bf4cef525761f1537117612
Chandi Kanhai <chandikanhai@gmail.com>
2018-01-26 09:52:51 -0500

    Fixes

--- src/connection.js
+++ src/connection.js
@@ -38,7 +38,7 @@ function onRobotConnection(connected) {
         address.disabled = connect.disabled = false;
         connect.textContent = 'Connect';
         // Add the default address and select xxxx
-        address.value = 'roborio-xxxx.local';
+        address.value = 'roboRIO-6504-FRC.local';
         address.focus();
         address.setSelectionRange(8, 12);
         // On click try to connect and disable the input and the button
