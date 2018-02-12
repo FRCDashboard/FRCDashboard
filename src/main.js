59c34c59efb281d178242ef779e19892f3c73b40
Chris <chris2fourlaw@gmail.com>
2018-02-11 20:43:42 -0800

    Add electron-builder for packaging, move icons, and fix camera

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ function createWindow() {
     // It's best if the dashboard takes up as much space as possible without covering the DriverStation application.
     // The window is closed until the python server is ready
     show: false,
-    icon: `${__dirname}/../images/icon.png`,
+    icon: `${__dirname}/../build/icon.png`,
   });
   // Move window to top (left) of screen.
   mainWindow.setPosition(0, 0);
@@ -131,57 +131,6 @@ function createWindow() {
   });
 }
 function createCameraWindow() {
-    // Attempt to connect to the localhost
-    client.start((con, err) => {
-      const connectFunc = () => {
-        console.log('Sending status');
-        cameraWindow.webContents.send('connected', con);
-
-        // Listens to the changes coming from the client
-      };
-
-      // If the Window is ready than send the connection status to it
-      if (ready) {
-        connectFunc();
-      }
-      connectedFunc = connectFunc;
-    });
-    // When the script starts running in the window set the ready variable
-    ipc.on('ready', (ev, mesg) => {
-      console.log('NetworkTables is ready');
-      ready = cameraWindow != null;
-
-      // Remove old Listener
-      client.removeListener(clientDataListener);
-
-      // Add new listener with immediate callback
-      client.addListener(clientDataListener, true);
-
-      // Send connection message to the window if if the message is ready
-      if (connectedFunc) connectedFunc();
-    });
-    // When the user chooses the address of the bot than try to connect
-    ipc.on('connect', (ev, address, port) => {
-      console.log(`Trying to connect to ${address}${port ? `:${port}` : ''}`);
-      const callback = (connected, err) => {
-        console.log('Sending status');
-        cameraWindow.webContents.send('connected', connected);
-      };
-      if (port) {
-        client.start(callback, address, port);
-      } else {
-        client.start(callback, address);
-      }
-    });
-    ipc.on('add', (ev, mesg) => {
-      client.Assign(mesg.val, mesg.key, (mesg.flags & 1) === 1);
-    });
-    ipc.on('update', (ev, mesg) => {
-      client.Update(mesg.id, mesg.val);
-    });
-    ipc.on('error', (ev, error) => {
-      console.log(error);
-    });
     // Create the browser window.
     cameraWindow = new BrowserWindow({
       width: 1280,
@@ -190,7 +139,7 @@ function createCameraWindow() {
       // It's best if the dashboard takes up as much space as possible without covering the DriverStation application.
       // The window is closed until the python server is ready
       show: false,
-      icon: `${__dirname}/../images/icon.png`,
+      icon: `${__dirname}/../build/icon.png`,
     });
     // Move window to top (left) of screen.
     cameraWindow.setPosition(0, 0);
@@ -198,7 +147,7 @@ function createCameraWindow() {
     cameraWindow.loadURL(`file://${__dirname}/../camera/src/index.html`);
     // Once the python server is ready, load window contents.
     cameraWindow.once('ready-to-show', () => {
-      console.log('main window is ready to be shown');
+      console.log('camera window is ready to be shown');
       cameraWindow.show();
     });
 
@@ -206,7 +155,7 @@ function createCameraWindow() {
     // cameraWindow.setMenu(null);
     // Emitted when the window is closed.
     cameraWindow.on('closed', () => {
-      console.log('main window closed');
+      console.log('camera window closed');
       // Dereference the window object, usually you would store windows
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
@@ -216,10 +165,10 @@ function createCameraWindow() {
       client.removeListener(clientDataListener);
     });
     cameraWindow.on('unresponsive', () => {
-      console.log('Main Window is unresponsive');
+      console.log('Camera Window is unresponsive');
     });
     cameraWindow.webContents.on('did-fail-load', () => {
-      console.log('window failed load');
+      console.log('camera window failed load');
     });
 }
 // This method will be called when Electron has finished
