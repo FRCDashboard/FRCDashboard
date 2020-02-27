e9d866d2a7b0ab199fad7e7cf08db175187ddbf5
CrispyBacon1999 <bacon.josh09@gmail.com>
2020-02-27 17:17:01 -0500

    Auto

--- test-robot-nt/autonomous/basic.py
+++ test-robot-nt/autonomous/basic.py
@@ -3,12 +3,16 @@ from magicbot import AutonomousStateMachine, timed_state
 from components.shooter import Shooter
 
 
-def Test1(AutonomousStateMachine):
-    MODE_NAME = "Test Autonomous Mode #1"
+def Basic(AutonomousStateMachine):
+    MODE_NAME = "Mode 1"
     DEFAULT = True
 
     shooter: Shooter
 
-    @timed_state(duration=10, first=True, next_state="done")
+    @timed_state(duration=10, first=True, next_state="test")
     def start(self):
         self.shooter.shoot()
+
+    @timed_state(duration=3)
+    def test(self):
+        pass
